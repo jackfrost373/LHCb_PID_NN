a191ef7f0d305acc2cafe5897812f5802750423a
Jacco de Vries <jdevries@nikhef.nl>
2018-12-24 21:53:01 +0100

    added MC script

---
+++ datafiles/data/MC17_MagDown_Nu1.6-25ns-Pythia8_Sim09e_Reco17_11144001/IOhelper.py
@@ -0,0 +1,6 @@
+from Gaudi.Configuration import *
+from GaudiConf import IOHelper
+IOHelper('ROOT').inputFiles(['PFN:./data/MC17_MagDown_Nu1.6-25ns-Pythia8_Sim09e_Reco17_11144001/00083523_00000001_5.ldst'], clear=True)
+
+# This is Bd -> JPsiKst
+
