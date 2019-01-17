8b7aa39d7fd811e88450cb9a054de95edae5740a
nelson <nelson.waakopreijers2@gmail.com>
2019-01-17 15:42:34 +0100

    Data_Handler updated such that hopefully the keys work. Don't mind the extra commented code, that is something I'm working on for the future.

--- Data_handling/Data_Handler.py
+++ Data_handling/Data_Handler.py
@@ -16,7 +16,7 @@ kaon_RICH = kaon_dataframe[['RichUsedAero', 'RichUsedR1Gas', 'RichUsedR2Gas', 'R
 kaon_CALO = kaon_dataframe[['EcalPIDe', 'EcalPIDmu', 'HcalPIDe', 'HcalPIDmu', 'PrsPIDe', 'InAccBrem', 'BremPIDe']]
 kaon_VELO = kaon_dataframe[['VeloCharge']]
 kaon_data = pd.concat([kaon_tracking, kaon_RICH, kaon_CALO, kaon_VELO], axis = 1)
-kaon_hdf5 = kaon_data.to_hdf('kaon_data.h5', key = 'kaon', format = 'table')
+kaon_hdf5 = kaon_data.to_hdf('particle_data.h5', key = 'kaon', format = 'table')
 
 # Extract neural network data for pions.
 pion_tree = file["DzTree_Pion/DecayTree"] # Select data to pick variables from.
@@ -27,7 +27,7 @@ pion_RICH = pion_dataframe[['RichUsedAero', 'RichUsedR1Gas', 'RichUsedR2Gas', 'R
 pion_CALO = pion_dataframe[['EcalPIDe', 'EcalPIDmu', 'HcalPIDe', 'HcalPIDmu', 'PrsPIDe', 'InAccBrem', 'BremPIDe']]
 pion_VELO = pion_dataframe[['VeloCharge']]
 pion_data = pd.concat([pion_tracking, pion_RICH, pion_CALO, pion_VELO], axis = 1)
-#pion_data
+pion_hdf5 = pion_data.to_hdf('particle_data.h5', key = 'pion', format = 'table')
 
 # Extract neural network data for protons.
 proton_tree = file["LcTree_Proton/DecayTree"] # Select data to pick variables from.
@@ -38,4 +38,19 @@ proton_RICH = proton_dataframe[['RichUsedAero', 'RichUsedR1Gas', 'RichUsedR2Gas'
 proton_CALO = proton_dataframe[['EcalPIDe', 'EcalPIDmu', 'HcalPIDe', 'HcalPIDmu', 'PrsPIDe', 'InAccBrem', 'BremPIDe']]
 proton_VELO = proton_dataframe[['VeloCharge']]
 proton_data = pd.concat([proton_tracking, proton_RICH, proton_CALO, proton_VELO], axis = 1)
-#proton_data
+proton_data
+
+#folder = sys.argv[1]
+
+#def NNdata (num): # Opens a .root file, makes it a dataframe and extracts the values we want for the neural network.
+#    file = uproot.open('/home/Shared/lhcbdata/ganga/20/'+num+'/output/davinci_MC_PID.root') # Open file.
+#    tree = file["PiTree/DecayTree"] #Access decay tree.
+#    df = tree.pandas.df() # Turn the ROOTDirectory into a dataframe.
+#    tracking = df[['TrackP', 'TrackPt', 'TrackChi2PerDof', 'TrackLikelihood', 'TrackGhostProbability', 'TrackFitMatchChi2', 'TrackCloneDist', 'TrackFitVeloChi2', 'TrackFitVeloNDoF', 'TrackFitTChi2', 'TrackFitTNDoF']] # Wanted variables from the tracker.
+#    RICH = df[['RichUsedAero', 'RichUsedR1Gas', 'RichUsedR2Gas', 'RichAboveMuThres', 'RichAboveKaThres', 'RichDLLe', 'RichDLLmu', 'RichDLLmu', 'RichDLLk', 'RichDLLp', 'RichDLLbt']] # Wanted data from the RICH detector.
+#    CALO = df[['EcalPIDe', 'EcalPIDmu', 'HcalPIDe', 'HcalPIDmu', 'PrsPIDe', 'InAccBrem', 'BremPIDe']] # Wanted data from the CALO detector.
+#    VELO = df[['VeloCharge']] # Wanted data from the VELO.
+#    data = pd.concat([tracking, RICH, CALO, VELO], axis = 1) # Strings all the variables together into 1 dataframe.
+#    return data
+
+
