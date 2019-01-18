841743c7d81ddaa67058e19eb11cb1fa80e7fdd6
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2019-01-18 15:53:45 +0100

    tensorboard graphics implemented

--- Main Networks/Tensorflow/PID_network/network_pid.py
+++ Main Networks/Tensorflow/PID_network/network_pid.py
@@ -2,10 +2,14 @@ import tensorflow as tf
 import pandas
 import numpy as np
 from tensorflow import keras
+from time import time
+from tensorflow.python.keras.callbacks import TensorBoard
+
+
 def sigmoid(x):
   return 1 / (1 + np.exp(-x))
 
-CATEBORIES = ["Pion", "Kaon"]
+NAME = "network_pid".format(int(time()))
 X = pandas.read_hdf("/home/felix/PycharmProjects/tensorflow_network/particle_data_big.h5")
 y = X.pi_TRUEID
 X= X.drop("pi_TRUEID", axis = 1)
@@ -42,6 +46,8 @@ model = keras.Sequential([
     keras.layers.Dense(2, activation=tf.nn.sigmoid)
 ])
 
+tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+
 # sets optimization function, learning rate and loss function for network
 model.compile(optimizer= tf.train.AdamOptimizer(),
               loss = 'sparse_categorical_crossentropy',
@@ -51,10 +57,27 @@ model.compile(optimizer= tf.train.AdamOptimizer(),
 model.fit(X,
           y,
           epochs = 1000,
-          batch_size=1000,
+          callbacks=[tensorboard],
+          batch_size=100,
           shuffle=True,
           validation_split=0.1
           )
+
+
+def variable_summaries(var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
+
+
 
 predictions = model.predict(X)
 # print(predictions)
@@ -74,3 +97,4 @@ for i in predictions:
 #confusion_matrix[0][9] = 'test'
 confusion_matrix = np.array(confusion_matrix)
 print(confusion_matrix)
+
